b2f6c1a1d4e9d2515eda4c78c80375eafb05076e
dodo <1524655672@qq.com>
2021-06-01 22:10:02 +0800

    2021-6-1 22.07 commit describe:完善jdbc连接数据库，进行增删查改的功能，并在查询后接受数据返回，在控制台打印

--- src/Test_jdbc.java
+++ src/Test_jdbc.java
@@ -1,9 +1,11 @@
+import com.mysql.cj.x.protobuf.MysqlxCrud;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class Test {
+public class Test_jdbc {
     public static final String URL="jdbc:mysql://localhost:3306/student1";
     public static final String USER="root";
     public static final String PASSWORD="dodo";
@@ -15,6 +17,12 @@ public class Test {
         Connection conn = DriverManager.getConnection(URL,USER,PASSWORD);
         //操作数据库，实现增删查改
         Statement stmt=conn.createStatement();
+        //增加向student表中增加两个字段
+        stmt.execute("insert into student values (13,'李小','17693360684','河北邦德','2012-02-12','男')");
+        stmt.execute("insert into student values (14,'陈大','17693360684','河北邦德','2012-02-12','女')");
+        //在student表中带条件的修改一个字段的值
+        stmt.execute("update student set phone_num='13728637286' where id=14");
+        //查询数据并返回数据
         ResultSet rs=stmt.executeQuery("SELECT id,name,phone_num,address,brithday,sex FROM student");
         //如果有数据，rs.next()返回true
         System.out.println("序号   姓名   电话号码  地址  生日  性别");
